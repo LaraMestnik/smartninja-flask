5aaff1e941bc92db752afc50a380046678b1995d
Lara Mestnik <lara.mestnik@gmail.com>
2020-02-05 10:28:18 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+import datetime
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    some_text = "Message from the handler."
+    current_year = datetime.datetime.now().year
+
+    cities = ["Boston", "Vienna", "Paris", "Berlin"]
+
+    return render_template("index.html", some_text=some_text, current_year=current_year, cities=cities)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/base")
+def base():
+    return render_template("base.html")
+
+
+if __name__ == '__main__':
+    app.run()
